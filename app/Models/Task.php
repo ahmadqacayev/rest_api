88e933d35b42419db5d7790c7abe55e4e714051e
Ahmad <ehmedqacayev@gmail.com>
2023-05-30 14:09:02 +0400

    rest_api

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 
 class Task extends Model
@@ -15,7 +15,7 @@ class Task extends Model
     protected $fillable = [
         'title',
         'is_done',
-        'project_id'
+        'project_id',
     ];
     protected $casts = [
         'is_done' => 'boolean'
@@ -31,8 +31,9 @@ class Task extends Model
     }
 
     protected static  function booted( ): void{
-        static::addGlobalScope('creator', function (Builder $builder){
-            $builder->where('creator_id', Auth::id());
+        static::addGlobalScope('member', function (Builder $builder){
+            $builder->where('creator_id', Auth::id())
+            ->orWhereIn('project_id', Auth::user()->memberships->pluck('id'));
         });
     }
 }
